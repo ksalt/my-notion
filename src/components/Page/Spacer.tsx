7b6ae39062a962730315d915135aea27e547dca9
ksalt <konstantyn.sk@gmail.com>
2026-01-19 23:05:43 +0100

    Spacer component

---
+++ src/components/Page/Spacer.tsx
@@ -0,0 +1,17 @@
+import styles from './Spacer.module.css';
+
+type SpacerProps = {
+  handleClick(): void;
+  showHint: boolean;
+}
+
+export const Spacer = ({ handleClick, showHint }: SpacerProps) => {
+  return (
+    <div
+      className={styles.spacer}
+      onClick={handleClick}
+    >
+      {showHint && "Click to create the first paragraph"}
+    </div>
+  );
+}
